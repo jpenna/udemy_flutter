a344d686b46f5c3c2eec8e086e3ca5ce1a38ba92
Juliano Penna <julianopenna@gmail.com>
2023-10-24 11:34:49 -0300

    Building form

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,58 @@
+import 'package:first_app/widgets/expenses_list/expenses_list.dart';
+import 'package:first_app/models/expense.dart';
+import 'package:first_app/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Restaurant Macarron',
+      amount: 34.50,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expenses'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}
