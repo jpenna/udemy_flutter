f1fc41642c1404d525da47ea270790b1192b7085
Juliano Penna <julianopenna@gmail.com>
2023-11-18 10:36:33 -0300

    Use SQLite

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,14 +1,70 @@
 import 'dart:io';
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:favorite_places/models/place.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as sys_path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
+const String _dbPath = 'user_places.db';
+const String _tableName = 'user_places';
+
+Future<Database> _getDb() async {
+  Database database = await sql.openDatabase(
+    _dbPath,
+    version: 1,
+    onCreate: (Database db, int version) async {
+      // When creating the db, create the table
+      await db.execute('CREATE TABLE $_tableName'
+          '(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+    },
+  );
+  return database;
+}
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final newPlace = Place(title: title, image: image, location: location);
+  Future<void> loadPlaces() async {
+    final db = await _getDb();
+
+    final data = await db.query(_tableName);
+    state = data
+        .map(
+          (row) => Place(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            location: PlaceLocation(
+              latitude: row['lat'] as double,
+              longitude: row['lng'] as double,
+              address: row['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+  }
+
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await sys_path.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
+
+    final db = await _getDb();
+
+    db.insert(_tableName, {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latitude,
+      'lng': newPlace.location.longitude,
+      'address': newPlace.location.address,
+    });
+
     state = [newPlace, ...state];
   }
 }
--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,15 +1,29 @@
+import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/screens/add_place.dart';
+import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:favorite_places/screens/add_place.dart';
-import 'package:favorite_places/widgets/places_list.dart';
-import 'package:favorite_places/providers/user_places.dart';
-
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<PlacesScreen> createState() {
+    return _PlacesScreenState();
+  }
+}
+
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _loadPlaces;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPlaces = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final userPlaces = ref.watch(userPlacesProvider);
 
     return Scaffold(
@@ -30,8 +44,14 @@ class PlacesScreen extends ConsumerWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: PlacesList(
-          places: userPlaces,
+        child: FutureBuilder(
+          future: _loadPlaces,
+          builder: (context, snapshot) =>
+              snapshot.connectionState == ConnectionState.waiting
+                  ? const Center(child: CircularProgressIndicator())
+                  : PlacesList(
+                      places: userPlaces,
+                    ),
         ),
       ),
     );
